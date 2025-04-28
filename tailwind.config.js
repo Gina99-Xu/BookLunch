ffae7c88833bcad948d0a77d032cc3fe3a08d34d
geoxy <ginnageoxy@gmail.com>
2025-04-28 21:21:34 +0800

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,26 +9,4 @@ module.exports = {
   theme: {
     extends: {}
   },
-
-  // plugins: [require('daisyui'), require('@tailwindcss/typography')],
-
-  // daisyui: {
-  //   themes: [
-  //     {
-  //       retro: {
-  //         "primary": "#FFD58E",  // Peach
-  //         "secondary": "#7EB7B3", // Teal
-  //         "accent": "#E8A2AF",    // Pink
-  //         "neutral": "#2A2520",   // Dark Brown
-  //         "base-100": "#F9E2BB",  // Cream
-  //         "info": "#A0C4E2",      // Light Blue
-  //         "success": "#B5EAD7",   // Mint
-  //         "warning": "#FFDAC1",   // Light Orange
-  //         "error": "#FF9AA2",     // Coral
-  //       },
-  //     },
-  //   ],
-  // }
-
-
 };
